be9b2f7d8129e5ec21517de33f41e24b50477f86
fellix desuza <fe11x@outlook.com>
2024-02-14 21:57:08 +0400

    Add Calculator class with three method GetNumber, GetOperator and PerformOperation

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public double GetNumber(string message)
+        {
+            Console.WriteLine(message);
+            double number;
+            if (!double.TryParse(Console.ReadLine(), out number))
+            {
+                throw new FormatException("Invalid input! Please enter a valid number.");
+            }
+            return number;
+        }
+
+        public char GetOperator(string message)
+        {
+            Console.WriteLine(message);
+            char op = Console.ReadKey().KeyChar;
+            if (op != '+' && op != '-' && op != '*' && op != '/')
+            {
+                throw new InvalidOperationException("Invalid operator!");
+            }
+            Console.WriteLine();
+            return op;
+        }
+
+        public double PerformOperation(double num1, char op, double num2)
+        {
+            switch (op)
+            {
+                case '+':
+                    return num1 + num2;
+                case '-':
+                    return num1 - num2;
+                case '*':
+                    return num1 * num2;
+                case '/':
+                    if (num2 == 0)
+                    {
+                        throw new DivideByZeroException("Division by zero!");
+                    }
+                    return num1 / num2;
+                default:
+                    throw new InvalidOperationException("Invalid operator!");
+            }
+        }
+    }
+}
