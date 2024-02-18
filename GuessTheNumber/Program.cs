6094ecd97d8d97ccc6702a086b70678d6caae2d7
fellix desuza <fe11x@outlook.com>
2024-02-18 18:30:42 +0400

    Add Second project "Guess The Number" game.

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace GuessTheNumber
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+               Console.WriteLine(" _____                       _____            _       ");
+               Console.WriteLine("|  __ \\                     / ____|          | |      ");
+               Console.WriteLine("| |__) |___  ___ ___  _ __| (___   ___  _ __| |_ ___ ");
+               Console.WriteLine("|  _  // _ \\/ __/ _ \\| '__|\\___ \\ / _ \\| '__| __/ __|");
+               Console.WriteLine("| | \\ \\  __/ (_| (_) | |   ____) | (_) | |  | |_\\__ \\");
+               Console.WriteLine("|_|  \\_\\___|\\___\\___/|_|  |_____/ \\___/|_|   \\__|___/");
+               Console.WriteLine("                                                       ");
+               Console.WriteLine("Welcome to \"Guess the Number!\"\n\n");
+               Console.WriteLine("Choose the game mode: \n" + "1 - Easy(from 1 to 25)\n" + "2 - Medium(from 1 to 50)\n" + "3 - Hard(from 1 to 100)\n");
+                
+            int mode = int.Parse(Console.ReadLine());
+
+            SecretNumber game = new SecretNumber(mode);
+
+            game.PlayGame();
+              
+           
+           
+            
+        }
+    }   
+}
---
+++ GuessTheNumber/SecretNumber.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheNumber
+{
+    public class SecretNumber
+    {
+        private int secretNumber;
+        public int answer { set; get; }
+        public SecretNumber(int gameDifficult)
+        {
+            Random random = new Random();
+
+
+            switch (gameDifficult)
+            {
+                case 1:
+                    secretNumber = random.Next(1, 25);
+                    break;
+                case 2:
+                    secretNumber = random.Next(1, 50);
+                    break;
+                case 3:
+                    secretNumber = random.Next(1, 100);
+                    break;
+                default:
+                    throw new ArgumentException("Invalid difficulty level. Please choose 1, 2, or 3.");
+            }
+        }
+
+
+        public void PlayGame()
+        {
+            Console.WriteLine("Well done!\nNow guess the number. You have 10 try.");
+            try
+            {
+
+                for (int i = 0; i < 10; i++)
+                {
+                    answer = int.Parse(Console.ReadLine());
+                    if (answer == secretNumber)
+                    {
+                        Console.WriteLine("Wow, Congratulations! You are Genius!\nSecret Number was: " + secretNumber);
+                        return;
+                    }
+                    else if (answer < secretNumber)
+                    {
+                        Console.WriteLine("The answer is less than the imagined number\nyou have " + (9 - i) + " try!");
+                    }
+                    else if (answer > secretNumber)
+                    {
+                        Console.WriteLine("The answer is more than the imagined number\nyou have " + (9 - i) + " try!");
+                    }
+                }
+                Console.WriteLine("Game Over.\n\nSecret Number was " + secretNumber);
+            }
+            catch (Exception)
+            {
+
+                throw new FormatException("Invalid input! Please enter only number.");
+            }
+
+        }
+    }
+}
+
